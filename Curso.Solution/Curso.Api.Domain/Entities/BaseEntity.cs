65e4622df18d5d1e4eef6506ed5d8affe65a1526
Alexandre Tavano <AlexandreTavanoDeveloper@outlook.com>
2021-05-04 19:45:28 -0300

    BaseEntity - UserEntity

---
+++ Curso.Solution/Curso.Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Curso.Api.Domain.Entities
+{
+    // Tabela geral de entitidade - Padrão para todas tabelas
+    public abstract class BaseEntity
+    {
+        private DateTime? _createAt;
+
+        [Key]
+        public Guid Id { get; set; }
+
+
+        // Retorna o valor para a CreateAt
+        public DateTime? CreateAt
+        {
+            get { return _createAt; }
+            set { _createAt = value; } // Como pode ser Nullo, caso for recebe o Datetime,Now
+        }
+        
+        public DateTime? UpdateAt { get; set; }
+    }
+}
